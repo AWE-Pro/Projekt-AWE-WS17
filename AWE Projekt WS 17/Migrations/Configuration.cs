a2deb310e50ffd492088849373e97a6a615917bb
s348817 <s348817@uniwuerzburg.onmicrosoft.com>
2018-02-25 22:00:58 +0100

    Hübsche AWE seed

--- AWE Projekt WS 17/Migrations/Configuration.cs
+++ AWE Projekt WS 17/Migrations/Configuration.cs
@@ -21,7 +21,8 @@ namespace AWE_Projekt_WS_17.Migrations
             {
                 new Course { Title = "Volkswirtschaftslehre", Description = "Angebot und Nachfrage", Owner = "testperson" },
                 new Course { Title = "Algorithmen und Datenstrukturen", Description = "Programmier Basics Java", Owner = "testperson2" },
-                new Course { Title = "Data Mining", Description = "Mining Methoden und Neuronale Netzwerke", Owner = "testperson3"  }
+                new Course { Title = "Data Mining", Description = "Mining Methoden und Neuronale Netzwerke", Owner = "testperson3"  },
+                new Course { Title = "Advanced Web-Engineering", Description = "Entwicklung von Anwendungen mit ASP.NET", Owner = "testperson4" }
             };
             courses.ForEach(s => context.Courses.AddOrUpdate(p => p.Title, s));
             context.SaveChanges();
@@ -31,7 +32,8 @@ namespace AWE_Projekt_WS_17.Migrations
                 new Tag {Name ="Awesome"},
                 new Tag {Name ="Bad"},
                 new Tag {Name ="Boring"},
-                new Tag {Name ="Funny"}
+                new Tag {Name ="Funny"},
+                new Tag {Name ="Awe"}
             };
             tags.ForEach(s => context.Tags.AddOrUpdate(p => p.Name, s));
             context.SaveChanges();
@@ -54,7 +56,10 @@ namespace AWE_Projekt_WS_17.Migrations
                 new ContentGroup {CourseID = 2, Order = 2, Header = "Programmieren"},
                 new ContentGroup {CourseID = 2, Order = 1, Header = "Algorithmen"},
                 new ContentGroup {CourseID = 3, Order = 2, Header = "Methoden"},
-                new ContentGroup {CourseID = 3, Order = 1, Header = "Formeln"}
+                new ContentGroup {CourseID = 3, Order = 1, Header = "Formeln"},
+                new ContentGroup {CourseID = 4, Order = 1, Header = "Controllers & Views"},
+                new ContentGroup {CourseID = 4, Order = 2, Header = "Client Components"}
+
             };
             contentGroups.ForEach(s => context.ContentGroups.AddOrUpdate(p => p.Header, s));
             context.SaveChanges();
@@ -68,7 +73,13 @@ namespace AWE_Projekt_WS_17.Migrations
                 new ContentElement {ContentID = 5, Description ="Prüfungsplan", Url ="https://www.uni-wuerzburg.de/fileadmin/32020000/WiWi-Bach/20172_2_Februar_PPlan_BA_ZR.pdf",TypeID=2, Order=1},
                 new ContentElement {ContentID = 6, Description ="Das schlimmste am Fach", Url ="Neuronale Netzwerke",TypeID=3, Order=1},
                 new ContentElement {ContentID = 6, Description ="Während der Klausur", Url ="https://www.youtube.com/embed/77sS5IuR0Gs",TypeID=1, Order=2},
-                new ContentElement {ContentID = 2, Description ="Zusammenfassung", Url ="Wo schneiden sich Angebots- und Nachfragekurve?",TypeID=3, Order=1}
+                new ContentElement {ContentID = 2, Description ="Zusammenfassung", Url ="Wo schneiden sich Angebots- und Nachfragekurve?",TypeID=3, Order=1},
+                new ContentElement {ContentID = 7, Description ="Aufbau eines MVC-Projekts", Url ="https://www.youtube.com/embed/f2s4k7q5Hd4",TypeID=1, Order=1},
+                new ContentElement {ContentID = 7, Description ="Web-Formulare", Url ="https://www.youtube.com/embed/A-wgEGurKGw",TypeID=1, Order=2},
+                new ContentElement {ContentID = 7, Description ="Aufgabenblatt", Url ="https://wuecampus2.uni-wuerzburg.de/moodle/pluginfile.php/1142027/mod_resource/content/0/AWE_Aufgabenblatt_3%2001%20FTH.pdf",TypeID=2, Order=3},
+                new ContentElement {ContentID = 8, Description ="Kendo UI Components", Url ="https://www.youtube.com/embed/0QAEBbGwINk",TypeID=1, Order=1},
+                new ContentElement {ContentID = 8, Description ="Kendo AutoComplete mit Server-Filterung", Url ="https://www.youtube.com/embed/YQNg4PwFLbk",TypeID=1, Order=2},
+                new ContentElement {ContentID = 8, Description ="Hinweis", Url ="Kendo UI macht viele Probleme!",TypeID=3, Order=3}
             };
             contentElements.ForEach(s => context.ContentElements.AddOrUpdate(p => p.Description, s));
             context.SaveChanges();
@@ -77,6 +88,7 @@ namespace AWE_Projekt_WS_17.Migrations
             AddOrUpdateTag(context, "Volkswirtschaftslehre", "Boring");
             AddOrUpdateTag(context, "Algorithmen und Datenstrukturen", "Boring");
             AddOrUpdateTag(context, "Data Mining", "Funny");
+            AddOrUpdateTag(context, "Advanced Web-Engineering", "Awe");
         }
 
         void AddOrUpdateTag(ApplicationDbContext context, string courseTitle, string tagName)
